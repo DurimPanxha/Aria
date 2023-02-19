0b432dc9bf6042b016fe1a487929ff1942b7afc3
Durim <durim.panxha1@gmail.com>
2023-02-19 23:52:09 +0100

    f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "604f66770548f35dae643cc1c279b7b1",
-"/": "604f66770548f35dae643cc1c279b7b1",
+"index.html": "03ada14ab4f8d5a57bb4dc34448e35f5",
+"/": "03ada14ab4f8d5a57bb4dc34448e35f5",
 "main.dart.js": "100fdd8a4ff83818dbf3719e4b5158aa",
 "manifest.json": "26fa68805a4b77d20b0d13e7fef29e9d",
 "version.json": "95eccd7b265b1730609fc9544c5b06c8"
